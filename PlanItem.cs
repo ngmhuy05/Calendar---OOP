e236e47ac280849c30aba2fe01fc3e872ca364a6
ThojTran <trantho18082005@gmail.com>
2024-11-03 17:49:05 +0700

    PlanItem.cs

--- PlanItem.cs
+++ PlanItem.cs
@@ -1,12 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ThiCuoiKy
 {
+
     public interface IPlanItem
     {
         DateTime Date { get; set; }
@@ -18,7 +20,7 @@ namespace ThiCuoiKy
     }
 
     [Serializable]
-    public abstract class BasePlanItem : IPlanItem
+    public class PlanItem : IPlanItem
     {
         private DateTime date;
         private string job;
@@ -29,188 +31,91 @@ namespace ThiCuoiKy
 
         public static List<string> liststatus = new List<string>() { "Done", "Late", "Coming", "Doing" };
 
-        public DateTime Date
-        {
-            get { return date; }
-            set { date = value; }
-        }
+        public DateTime Date { get => date; set => date = value; }
+        public string Job { get => job; set => job = value; }
+        public Point FromTime { get => fromTime; set => fromTime = value; }
+        public Point ToTime { get => toTime; set => toTime = value; }
+        public string Status { get => status; set => status = value; }
+        public string Note1 { get => Note; set => Note = value; }
 
-        public string Job
-        {
-            get { return job; }
-            set { job = value; }
-        }
-
-        public Point FromTime
-        {
-            get { return fromTime; }
-            set { fromTime = value; }
-        }
-
-        public Point ToTime
-        {
-            get { return toTime; }
-            set { toTime = value; }
-        }
-
-        public string Status
-        {
-            get { return status; }
-            set { status = value; }
-        }
-
-        public string Note1
-        {
-            get { return Note; }
-            set { Note = value; }
-        }
-
-        protected BasePlanItem()
-        {
-            date = DateTime.Now;
-        }
-
-        public virtual void UpdateStatus(StatusEnum newStatus)
-        {
-            status = liststatus[(int)newStatus];
-        }
-
-        public virtual int GetDuration()
-        {
-            return (ToTime.X * 60 + ToTime.Y) - (FromTime.X * 60 + FromTime.Y);
-        }
-
-        public abstract bool IsCompleted();
     }
-
     [Serializable]
-    public class PlanItem : BasePlanItem
+    public class RecurringPlanItem : PlanItem
     {
-        public PlanItem() : base()
+        private int repeatInterval;
+        private static List<RecurringPlanItem> allRecurringItems = new List<RecurringPlanItem>();
+
+        public class DuplicateJobException : Exception
         {
+            public DuplicateJobException(string message) : base(message) { }
         }
 
-        public PlanItem(DateTime date, string job, Point fromTime, Point toTime)
+        public int RepeatInterval
+        {
+            get => repeatInterval;
+            set => repeatInterval = value;
+        }
+
+        // Kiểm tra xem có công việc trùng lặp không
+        private bool HasDuplicateJob(DateTime date, string job)
+        {
+            for (int i = 0; i < allRecurringItems.Count; i++)
+            {
+                RecurringPlanItem item = allRecurringItems[i];
+                if (item.Date.Date == date.Date && item.Job.ToLower() == job.ToLower())
+                {
+                    throw new DuplicateJobException($"Công việc '{job}' bị trùng lặp trong cùng 1 ngày ");
+                }
+            }
+            return false;
+        }
+
+        // Thêm công việc mới với kiểm tra trùng lặp
+        public void AddRecurringItem()
+        {
+            try
+            {
+                HasDuplicateJob(this.Date, this.Job);
+                allRecurringItems.Add(this);
+            }
+            catch (DuplicateJobException ex)
+            {
+                throw; // Ném lại ngoại lệ để lớp gọi có thể xử lý
+            }
+        }
+
+        // Lấy danh sách tất cả công việc
+        public static List<RecurringPlanItem> GetRecurringItems()
+        {
+            List<RecurringPlanItem> a = new List<RecurringPlanItem>();
+            for (int i = 0; i < allRecurringItems.Count; i++)
+            {
+                a.Add(allRecurringItems[i]);
+            }
+            return a;
+        }
+
+        // Constructor
+        public RecurringPlanItem()
+        {
+            repeatInterval = 1;
+        }
+
+        public RecurringPlanItem(DateTime date, string job, Point fromTime, Point toTime, int repeatInterval)
         {
             this.Date = date;
             this.Job = job;
             this.FromTime = fromTime;
             this.ToTime = toTime;
-            this.Status = liststatus[(int)StatusEnum.Coming];
-        }
-
-        public override bool IsCompleted()
-        {
-            return Status == liststatus[(int)StatusEnum.Done];
-        }
-
-        public bool IsDoing()
-        {
-            return Status == liststatus[(int)StatusEnum.Doing];
-        }
-
-        public bool IsLate()
-        {
-            return Status == liststatus[(int)StatusEnum.Late];
-        }
-
-        public bool IsComing()
-        {
-            return Status == liststatus[(int)StatusEnum.Coming];
-        }
-
-        public bool IsToday()
-        {
-            return Date.Date == DateTime.Now.Date;
+            this.repeatInterval = repeatInterval;
+            this.Status = "Coming";
         }
     }
-
-    [Serializable]
-        // Xử lý công việc lặp lại 
-    public class RecurringPlanItem : BasePlanItem
-    {
-        private DayOfWeek recurringDay;
-
-        public DayOfWeek RecurringDay
-        {
-            get { return recurringDay; }
-            set { recurringDay = value; }
-        }
-
-        public RecurringPlanItem() : base()
-        {
-            recurringDay = DateTime.Now.DayOfWeek;
-        }
-
-        public override bool IsCompleted()
-        {
-            return false; 
-        }
-
-        public bool IsRecurringDay()
-        {
-            return DateTime.Now.DayOfWeek == recurringDay;
-        }
-
-        public override void UpdateStatus(StatusEnum newStatus)
-        {
-            if (IsRecurringDay())
-            {
-                base.UpdateStatus(newStatus);
-            }
-        }
-    }
-
-    [Serializable]
-    // Thêm deadline khi mà công việc quá thời gian sẽ chuyển sang late 
-    public class UrgentPlanItem : BasePlanItem
-    {
-        private DateTime deadline;
-        private int priority;
-
-        public DateTime Deadline
-        {
-            get { return deadline; }
-            set { deadline = value; }
-        }
-
-        public int Priority
-        {
-            get { return priority; }
-            set { priority = value; }
-        }
-
-        public UrgentPlanItem() : base()
-        {
-            deadline = DateTime.Now.AddDays(1);
-            priority = 1;
-        }
-
-        public override bool IsCompleted()
-        {
-            return Status == liststatus[(int)StatusEnum.Done];
-        }
-
-        public override void UpdateStatus(StatusEnum newStatus)
-        {
-            base.UpdateStatus(newStatus);
-            if (DateTime.Now > Deadline && !IsCompleted())
-            {
-                Status = liststatus[(int)StatusEnum.Late];
-            }
-        }
-
-        public bool IsOverdue()
-        {
-            return DateTime.Now > Deadline && !IsCompleted();
-        }
-    }
-
     public enum StatusEnum
     {
         Done,
         Late,
         Coming,
-        Doing
+        Doing,
     }
 }
