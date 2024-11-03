4e9117365dcb6edf4f2444857a090541da825b30
ngmhuy05 <nguyenhuy142005@gmail.com>
2024-11-04 00:35:49 +0700

    NotificationBase.cs
    Thêm class NotificationBase.cs

---
+++ NotificationBase.cs
@@ -0,0 +1,22 @@
+using Calender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.Notify
+{
+    public abstract class NotificationBase
+    {
+        protected bool isEnabled;
+
+        protected NotificationBase()
+        {
+            this.isEnabled = false;
+        }
+
+        public abstract void ShowNotification(string message);
+        public abstract bool ShouldNotify(DateTime currentDate, List<PlanItem> jobs);
+    }
+}
