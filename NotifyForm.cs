52ff2255ea2ac2233f876bbd5afebe4b958750fc
ngmhuy05 <nguyenhuy142005@gmail.com>
2024-11-06 23:23:05 +0700

    Update NotifyForm.cs
    Thêm giải thích cho code

--- NotifyForm.cs
+++ NotifyForm.cs
@@ -10,23 +10,31 @@ using System.Windows.Forms;
 
 namespace ThiCuoiKy
 {
-        public partial class NotifyForm : Form
+    // Định nghĩa NotifyForm kế thừa từ Form để tạo một cửa sổ thông báo.
+    public partial class NotifyForm : Form
+    {
+        public NotifyForm(string message)
         {
-            public NotifyForm(string message)
-            {
-                InitializeComponent();
-                lbNotify.Text = message; 
-                btnOK.Click += new EventHandler(btnOK_Click);
-            }
+            // Khởi tạo các thành phần giao diện của form, bao gồm các control như lbNotify (label hiển thị thông báo) và btnOK (nút đóng).
+            InitializeComponent();
 
-            // Đóng thông báo khi nhấn nút OK
-            private void btnOK_Click(object sender, EventArgs e)
-            {
-                this.Close();
-            }
-        private void lbNotify_Click(object sender, EventArgs e)
-        {
+            // Gán chuỗi thông báo truyền vào (message) cho thuộc tính Text của lbNotify, để nội dung này xuất hiện trên form khi nó được hiển thị.
+            lbNotify.Text = message;
 
+            // Sự kiện Click của btnOK để khi nút này được nhấn, hàm btnOK_Click sẽ được gọi và thực thi hành động đóng form.
+            btnOK.Click += new EventHandler(btnOK_Click);
         }
+
+        // Đóng thông báo khi nhấn nút OK
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    // Tóm gọn: lớp NotifyForm hiển thị thông báo qua lbNotify, đăng ký sự kiện đóng khi nút OK được nhấn.
+    
+    private void lbNotify_Click(object sender, EventArgs e)
+    {
+
     }
 }
