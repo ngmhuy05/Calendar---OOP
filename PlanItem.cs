248f4a0822ec66d2a597ab2121d3e6aa75eedb00
ThojTran <trantho18082005@gmail.com>
2024-11-06 22:08:10 +0700

    New

--- PlanItem.cs
+++ PlanItem.cs
@@ -39,78 +39,7 @@ namespace ThiCuoiKy
         public string Note1 { get => Note; set => Note = value; }
 
     }
-    [Serializable]
-    public class RecurringPlanItem : PlanItem
-    {
-        private int repeatInterval;
-        private static List<RecurringPlanItem> allRecurringItems = new List<RecurringPlanItem>();
 
-        public class DuplicateJobException : Exception
-        {
-            public DuplicateJobException(string message) : base(message) { }
-        }
-
-        public int RepeatInterval
-        {
-            get => repeatInterval;
-            set => repeatInterval = value;
-        }
-
-        // Kiểm tra xem có công việc trùng lặp không
-        private bool HasDuplicateJob(DateTime date, string job)
-        {
-            for (int i = 0; i < allRecurringItems.Count; i++)
-            {
-                RecurringPlanItem item = allRecurringItems[i];
-                if (item.Date.Date == date.Date && item.Job.ToLower() == job.ToLower())
-                {
-                    throw new DuplicateJobException($"Công việc '{job}' bị trùng lặp trong cùng 1 ngày ");
-                }
-            }
-            return false;
-        }
-
-        // Thêm công việc mới với kiểm tra trùng lặp
-        public void AddRecurringItem()
-        {
-            try
-            {
-                HasDuplicateJob(this.Date, this.Job);
-                allRecurringItems.Add(this);
-            }
-            catch (DuplicateJobException ex)
-            {
-                throw; // Ném lại ngoại lệ để lớp gọi có thể xử lý
-            }
-        }
-
-        // Lấy danh sách tất cả công việc
-        public static List<RecurringPlanItem> GetRecurringItems()
-        {
-            List<RecurringPlanItem> a = new List<RecurringPlanItem>();
-            for (int i = 0; i < allRecurringItems.Count; i++)
-            {
-                a.Add(allRecurringItems[i]);
-            }
-            return a;
-        }
-
-        // Constructor
-        public RecurringPlanItem()
-        {
-            repeatInterval = 1;
-        }
-
-        public RecurringPlanItem(DateTime date, string job, Point fromTime, Point toTime, int repeatInterval)
-        {
-            this.Date = date;
-            this.Job = job;
-            this.FromTime = fromTime;
-            this.ToTime = toTime;
-            this.repeatInterval = repeatInterval;
-            this.Status = "Coming";
-        }
-    }
     public enum StatusEnum
     {
         Done,
